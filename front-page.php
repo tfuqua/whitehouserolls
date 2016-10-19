ce618b69f43697076aac4bef715af1ec43bb2267
Taylor Fuqua <tfuqua@gwu.edu>
2016-10-19 02:18:51 -0400

    Whitehouse Rolls style updates

--- front-page.php
+++ front-page.php
@@ -14,13 +14,20 @@ get_header(); ?>
     <?php if(get_field('hero_image')) { ?>
       <?php $background = wp_get_attachment_image_src(get_field('hero_image'), 'full', false); ?>
 	     <div class="hero hero-1" style="background-image: url('<?php echo $background[0] ?>');">
-        <div class="hero-text">
-          <?php echo get_field('hero_text')?>
-          <?php if(get_field('button_text')) { ?>
-          <div class="hero-button">
-            <a href="#"><?php echo get_field('button_text')?></a>
+        <div class="hero-text-wrapper">
+          <div>
+            <div class="hero-text">
+              <?php echo get_field('hero_text')?>
+              <div class="extra-img">
+                <?php echo wp_get_attachment_image(get_field('extra_image'), 'full', false, array( 'class' => '') ); ?>
+              </div>
+              <?php if(get_field('button_text')) { ?>
+              <div class="hero-button">
+                <a href="<?php echo get_field('button_link')?>"><?php echo get_field('button_text')?></a>
+              </div>
+              <?php } ?>
+            </div>
           </div>
-          <?php } ?>
         </div>
       </div>
     <?php } ?>
@@ -51,17 +58,23 @@ get_header(); ?>
             <?php while ( have_rows('featured_content') ) : the_row(); ?>
                 <div class="featured-item">
                   <div class="featured-section">
-                    <h3><?php echo the_sub_field('heading');?></h3>
-                    <div class="featured-body">
-                      <?php echo the_sub_field('body');?>
+                    <div class="flex">
+                      <h3><?php echo the_sub_field('heading');?></h3>
+                      <div class="featured-body">
+                        <?php echo the_sub_field('body');?>
+                      </div>
+                      <div class="featured-button">
+                        <a>
+                          <?php echo the_sub_field('button_text');?>
+                        </a>
+                      </div>
                     </div>
-                    <div class="featured-button">
-                      <a>
-                        <?php echo the_sub_field('button_text');?>
-                      </a>
+                    <?php if(get_sub_field('image')) { ?>
+                    <div class="img-wrapper flex">
+                      <?php echo wp_get_attachment_image(get_sub_field('image'), 'medium', false, array( 'class' => 'lazy-load'));?>
                     </div>
+                   <?php } ?>
                   </div>
-
                 </div>
             <?php endwhile; ?>
           </div>
@@ -73,16 +86,22 @@ get_header(); ?>
       <?php if(get_field('hero_2_image')) { ?>
       	<div class="hero hero-2">
           <div class="container-fluid">
-            <?php echo wp_get_attachment_image(get_field('hero_2_image'), 'full', false, array( 'class' => '') ); ?>
-
+            <div class="hero-img-wrapper">
+              <?php echo wp_get_attachment_image(get_field('hero_2_image'), 'full', false, array( 'class' => '') ); ?>
+            </div>
             <div class="hero-text">
-              <?php echo get_field('hero_2_text')?>
+              <h3>
+                <?php echo get_field('hero_header')?>
+              </h3>
+              <div>
+                <?php echo get_field('hero_2_text')?>
+              </div>
               <?php if(get_field('button_text')) { ?>
               <div class="hero-button">
-                <a href="#"><?php echo get_field('button_2_text')?></a>
+                <a href="<?php echo get_field('button_2_link')?>"><?php echo get_field('button_2_text')?></a>
               </div>
               <?php } ?>
-            </div>            
+            </div>
           </div>
         </div>
       <?php } ?>
